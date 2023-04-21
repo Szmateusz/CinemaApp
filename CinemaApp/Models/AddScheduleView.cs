a24e2d774b5afa7c65d5cf8f2b11ea39c5a482e3
Szmateusz <Szalast2003@wp.pl>
2023-04-21 21:42:04 +0200

    Add:admin panel func

---
+++ CinemaApp/Models/AddScheduleView.cs
@@ -0,0 +1,10 @@
+﻿namespace CinemaApp.Models
+{
+    public class AddScheduleView
+    {
+        public List<MovieModel> Movies { get; set; }
+        public List<HallModel> Halls { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+}
